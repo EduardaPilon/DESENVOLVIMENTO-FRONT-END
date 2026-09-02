e6325ced3a9417c5dfbd3dc740a725c74df70e3c
Eduarda <eduardapilonguarino@gmail.com>
2026-09-02 20:09:42 -0300

    api

--- js/api.js
+++ js/api.js
@@ -1,1 +1,24 @@
-fetch("dados.json")
+/*
+export = permite que outro arquivo JavaScript ultilize a função
+*/
+
+export async function carregarTarefas() {
+    try {
+        const resposta = await fetch("../dados.json");
+
+        if (!resposta.ok) {
+            throw new Error(`Erro HTTP: ${resposta.status}`);
+        }
+
+        const dados = await resposta.json();
+
+        if (!dados || !Array.isArray(dados.tarefas)) {
+            throw new Error("Formato inválido");
+        }
+
+        return dados.tarefas;
+
+    } catch (erro) {
+        throw erro;
+    }
+}
